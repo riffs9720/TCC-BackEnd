acb7045a281f621af065846189bc1ffbe650aa66
Riffs9720 <rferlasilva@gmail.com>
2020-05-26 11:18:27 -0300

    Configurações para deploy no heroku

--- config/database.js
+++ config/database.js
@@ -50,17 +50,17 @@ module.exports = {
   | npm i --save mysql
   |
   */
- mysql: {
-  client: 'mysql',
-  connection: {
-    host: Env.get('DB_HOST', DATABASE_URL.hostname),
-    port: Env.get('DB_PORT', DATABASE_URL.port),
-    user: Env.get('DB_USER', DATABASE_URL.username),
-    password: Env.get('DB_PASSWORD', DATABASE_URL.password),
-    database: Env.get('DB_DATABASE', DATABASE_URL.pathname.substr(1))
+  mysql: {
+    client: 'mysql',
+    connection: {
+      host: Env.get('DB_HOST', DATABASE_URL.hostname),
+      port: Env.get('DB_PORT', DATABASE_URL.port),
+      user: Env.get('DB_USER', DATABASE_URL.username),
+      password: Env.get('DB_PASSWORD', DATABASE_URL.password),
+      database: Env.get('DB_DATABASE', DATABASE_URL.pathname.substr(1))
+    },
+    debug: Env.get('DB_DEBUG', false)
   },
-  debug: Env.get('DB_DEBUG', false)
-}
 /*
 Variaveis para uso local
 mysql: {
