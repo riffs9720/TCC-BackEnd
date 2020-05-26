58491097883cad020804cfa06bf0f092cad98424
Riffs9720 <rferlasilva@gmail.com>
2020-05-26 12:02:37 -0300

    Configurado arquivos de acesso ao heroku

--- config/database.js
+++ config/database.js
@@ -53,11 +53,11 @@ module.exports = {
   mysql: {
     client: 'mysql',
     connection: {
-      host: Env.get(DB_HOST),
-      port: Env.get(DB_PORT),
-      user: Env.get(DB_USER),
-      password: Env.get(DB_PASSWORD),
-      database: Env.get(DB_DATABASE)
+      host: Env.get('DB_HOST', DB_HOST),
+      port: Env.get('DB_PORT', DB_PORT),
+      user: Env.get('DB_USER', DB_USER),
+      password: Env.get('DB_PASSWORD', DB_PASSWORD),
+      database: Env.get('DB_DATABASE', DB_DATABASE)
     },
     debug: Env.get('DB_DEBUG', false)
   },
